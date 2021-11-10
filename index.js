fface55ed058fbfe6a2490e35c4cfa90f0b7af3a
honeychuckles <kaylaedwards575@gmail.com>
2021-11-10 16:06:18 +0000

    finsihed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //write your code here
+function stripUpperCase(string){
+    let newString="";
+    for(let i =0; i < string.length; i++){
+        let letter = string[i];
+        if (letter.toLowerCase() === letter ){
+            newString += letter;
+        }
+             
+    }
+    
+    return newString;
+}
+
+console.log(stripUpperCase('Hello!')); // 'ello!'
+console.log(stripUpperCase('SevenEleven')); // 'evenleven'
+console.log(stripUpperCase("Don't play with Me!")); // 'ont play with e!'
